0f2ea709e2ea63b645f3ef73327030db20ecf866
Taisuke Fukuno <fukuno@jig.jp>
2021-05-03 09:04:10 +0900

    mod fetch_test

--- bench/fetch_test.js
+++ bench/fetch_test.js
@@ -1,6 +1,7 @@
 import { Pbf } from "https://taisukef.github.io/pbf/Pbf.js";
 import { Tile } from "https://taisukef.github.io/pbf/bench/vector_tile.js";
 
-const data = await (await fetch('https://taisukef.github.io/pbf/test/fixtures/12665.vector.pbf')).arrayBuffer();
+const url = "https://cyberjapandata.gsi.go.jp/xyz/experimental_bvmap/6/57/25.pbf";
+const data = await (await fetch(url)).arrayBuffer();
 const tile = Tile.read(new Pbf(data));
 console.log(tile);
